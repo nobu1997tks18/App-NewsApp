77080fc1147ae1459ecde5a87871842be8a70552
nobu1997tks18 <n.k.1997.tks.18@gmail.com>
2020-05-04 14:03:32 +0900

    modify front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/about'
+  get '/home', to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  resources :users
 end
