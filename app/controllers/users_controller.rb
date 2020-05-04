fbc6273a603d892c37f16e4039eb1a2aa8315335
nobu1997tks18 <n.k.1997.tks.18@gmail.com>
2020-05-04 15:38:14 +0900

    make create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
+  attr_accessor :name, :email
   def new
-    # @user = User.new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(params_user)
+    @user.save
+  end
+
+
+
+  private
+  def params_user
+    params.require(:user).permit(:name, :email)
   end
 end
