218f4cda239c342ca98d4dd94b38ed4c6f60aa86
nobu1997tks18 <n.k.1997.tks.18@gmail.com>
2020-05-05 17:30:12 +0900

    Create login logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      login @user
+      redirect_to @user
+    else
+      flash.now[:alert] = "入力情報を確認してください"
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_path
+  end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,20 @@
 module SessionsHelper
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  def logout
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
+  def user_login?
+    !current_user.nil?
+  end
 end
