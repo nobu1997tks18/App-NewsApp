9270ad4a80f8741fef54cf4c4e29f22fae86a71e
nobu1997tks18 <n.k.1997.tks.18@gmail.com>
2020-05-18 18:26:34 +0900

    modify seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,77 @@
 User.create(
-  name: "管理者ユーザー",
+  name: "金澤伸行",
   email: "nobu@test.com",
   password: "aaaaaaaa",
   password_confirmation: "aaaaaaaa",
-  profile: "ユーザ管理者です！是非このアプリを活用してください！",
+  profile: "プログラミング学習中です！ポートフォリオ一作目として作成しました！",
   admin: true
-
 )
+
 User.create(
   name: "田中太郎",
   email: "taro@test.com",
   password: "aaaaaaaa",
   password_confirmation: "aaaaaaaa",
-  profile: "プログラミング学習中です！ バックエンドエンジニアを目指しています！",
-)
+  profile: "プログラミング学習中です！バックエンドエンジニア目指して学習してます！"
+  )
 
 User.create(
   name: "田中次郎",
   email: "jiro@test.com",
   password: "aaaaaaaa",
   password_confirmation: "aaaaaaaa",
-  profile: "フロントエンジニアを目指して学習中です！ Javascriptが得意です！"
+  profile: "プログラミング学習中です！フロントエンドエンジン遺雨足て学習してます。JSが得意です！"
   )
-
-  
   
   %w[経済 ビジネス 政治 マネー スポーツ 海外 その他].each {|genre| Genre.create(name: genre)}
+
+20.times do 
+  name = Faker::Name.last_name
+  email = Faker::Internet.email
+  password = "password"
+  User.create(
+    name: name,
+    email: email,
+    password: password,
+    password_confirmation: password,
+    profile: "プログラミング学習中です！"
+  )
+end
+
+
+20.times do 
+  post_id = Faker::Number.within(range: 1..99)
+  user_id = Faker::Number.within(range: 1..99)
+  Comment.create(
+    content: "いい記事ですね！",
+    user_id: user_id,
+    post_id: post_id
+  )
+end
+
+20.times do 
+  post_id = Faker::Number.within(range: 1..99)
+  user_id = Faker::Number.within(range: 1..99)
+  Like.create(
+    user_id: user_id,
+    post_id: post_id
+  )
+end
+
+users = User.all
+user = users.first
+followings = users[2..50]
+followers = users[12..70]
+followings.each {|following| user.follow(following)}
+followers.each {|follower| follower.follow(user)}
+
+20.times do 
+  user_id = Faker::Number.within(range: 1..99)
+  genre_id = 1
+  post_id = Post.create(
+    content: "この記事は非常に参考になります",
+    url: "https://newspicks.com/",
+    user_id: user_id,
+    genre_ids: genre_id
+  )
+end
