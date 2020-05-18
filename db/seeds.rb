aacf720565c495e447f5dc49f323843e2cf59b4e
nobu1997tks18 <n.k.1997.tks.18@gmail.com>
2020-05-18 14:39:42 +0900

    modify seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,16 +3,26 @@ User.create(
   email: "nobu@test.com",
   password: "aaaaaaaa",
   password_confirmation: "aaaaaaaa",
-  profile: "プログラミング学習中です！",
+  profile: "ユーザ管理者です！是非このアプリを活用してください！",
   admin: true
+
+)
+User.create(
+  name: "田中太郎",
+  email: "nobu@test.com",
+  password: "aaaaaaaa",
+  password_confirmation: "aaaaaaaa",
+  profile: "プログラミング学習中です！ バックエンドエンジニアを目指しています！",
 )
 
 User.create(
-  name: "田中太郎",
+  name: "田中次郎",
   email: "taro@test.com",
   password: "aaaaaaaa",
   password_confirmation: "aaaaaaaa",
-  profile: "プログラミング学習中です！"
+  profile: "フロントエンジニアを目指して学習中です！ Javascriptが得意です！"
   )
+
+  
   
   %w[経済 ビジネス 政治 マネー スポーツ 海外 その他].each {|genre| Genre.create(name: genre)}
