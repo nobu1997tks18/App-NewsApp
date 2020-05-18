26a90ccdfd072096f8664e284077eb6fb325fb4a
nobu1997tks18 <n.k.1997.tks.18@gmail.com>
2020-05-18 18:23:00 +0900

    modify links

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -30,4 +30,12 @@ class NewsController < ApplicationController
     @domein = "soccer-king.jp"
     connecting_to_news_api
   end
+
+  private
+  def connecting_to_news_api
+    uri = URI.parse('http://newsapi.org/v2/everything?domains='+ @domein +'&apiKey=2f3f112d66d34c99963583f39c44d4ea')
+    json = Net::HTTP.get(uri)
+    moments = JSON.parse(json)
+    @moments = moments['articles']
+  end
 end
