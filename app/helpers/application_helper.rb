a009dedb80803d4bb7a2fb971c52a0f44a2b21df
nobu1997tks18 <n.k.1997.tks.18@gmail.com>
2020-05-23 15:09:50 +0900

    update gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def link_for_admin
+    if user_login?
+      unless @current_user.admin?
+        link_to "アカウント削除はこちら", user_path(@user), method: :delete, data: {confirm: "削除してよろしいですか？"}
+      end
+    end
+  end
 end
